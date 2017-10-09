5aa3b0ad4dc825a3f7ec890a2240cdc0045c842d
Patrick Assoa Adou <patrick.assoa.adou@gmail.com>
2017-10-09 14:40:15 +0200

    bug fix

--- src/RecastAIService.php
+++ src/RecastAIService.php
@@ -26,7 +26,7 @@ class RecastAIService {
      * 
      * @return Kanian\RecastAILaravel\RecastAIService
      */
-    public function get() : Kanian\RecastAILaravel\RecastAIService  {
+    public function get() : RecastAI\Client  {
         return $this->recastai;
     }
     
